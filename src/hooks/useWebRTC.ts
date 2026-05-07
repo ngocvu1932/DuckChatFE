451d1337caf40fafc398989a00bfc3e0ed8eabc1
NgocVu <ngongocvu668@gmail.com>
2026-05-07 15:25:35 +0700

    add video call --fix

--- src/hooks/useWebRTC.ts
+++ src/hooks/useWebRTC.ts
@@ -34,7 +34,14 @@ export const useWebRTC = (userId: string): WebRTCInstance => {
 
   const createPeer = () => {
     const pc = new RTCPeerConnection({
-      iceServers: [{urls: 'stun:stun.l.google.com:19302'}],
+      iceServers: [
+        {urls: 'stun:stun.l.google.com:19302'},
+        {
+          urls: 'turn:openrelay.metered.ca:80',
+          username: 'openrelayproject',
+          credential: 'openrelayproject',
+        },
+      ],
     });
 
     pc.ontrack = (e) => {
